123f8e60835d603f166a319f4b0eced4bfa50476
Vikash <bikash9090@gmail.com>
2016-09-16 09:07:30 +0530

    New design Changes

---
+++ app/scripts/directives/footer/footer.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+/**
+ * @ngdoc directive
+ * @name izzyposWebApp.directive:adminPosHeader
+ * @description
+ * # adminPosHeader
+ */
+angular.module('sbAdminApp')
+	.directive('footer', function () {
+	    return {
+	        templateUrl: 'scripts/directives/footer/footer.html',
+	        restrict: 'E',
+	        replace: true,
+	    }
+	});
